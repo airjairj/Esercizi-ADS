9e97843dfe46ededb9913bf730e52a430290dfba
Falino <alessandrofalino@gmail.com>
2023-10-29 16:13:43 +0100

    Implementato HW2

--- HW2/2.2/Soluzione_Es2.py
+++ HW2/2.2/Soluzione_Es2.py
@@ -1,117 +1,104 @@
-def trovaPosizioni(n):
-    scacchiera = [["." for _ in range(n)] for _ in range(n)]
-    combinazioni = 0
+class Output:
+    def numeroSoluzioni(self, n):
+        # Creo la scacchiera
+        scacchiera = [['.' for i in range(n)] for j in range(n)]
+        # Imposto il numero di regine da piazzare ad n
+        self.regine = n
+        # Inizializzo le combinazioni trovate a 0
+        self.combinazioni = 0
+        # Invoco la prova delle posizioni
+        self.provaPosizione(0, scacchiera)
+        # Ritorno le combinazioni trovate (modificate nella funzione "provaPosizione")
+        return self.combinazioni
+    
+    def provaPosizione(self, i, scacchiera, regine=None, piazzate=0):
+        """
+        Dati in input:
+        i (la riga corrente)
+        scacchiera (la configurazione corrente della scacchiera)
+        regine (una lista delle posizioni delle regine precedentemente piazzate, inizializzata a None)
+        piazzate (il numero di regine già piazzate, inizializzato a 0)
+        esegue un ciclo su tutte le colonne di una riga e cerca di piazzare una regina 
+        """
 
-    def provaCombinazione(comb,scacc):
-        for colonna in range(n):
-            colonna_iniziale = colonna
-            riga = 0
-            scacc = [["." for _ in range(n)] for _ in range(n)] # Reset
-            comb += piazzaRegine(riga,colonna_iniziale,n,scacc,n*n)
-        return comb
+        if piazzate == self.regine: # self.regine è n per intenderci
+            self.combinazioni += 1  # Se quindi ho piazzato n regine ho una combinazione valida
+            return
         
-    def piazzaRegine(r,c,n,s,rimasti):
-        print("PROVO")
-        piazzate = 0
-        spazi_rimasti = rimasti
-        # Trovato spazio libero
-        if s[r][c] == ".":
-            riga_t = r
-            colonna_t = c
+        if i>= self.regine:         # Se i >= self.regine (che è sempre n) significa che non ho piazzato nessuna regina sulla riga, quindi non cè soluzione
+            return                  # o qualcosa è andato storto perchè è finita la scacchiera
+        
+        if not regine:              # Inizializzo le posizioni se non l'ho gia fatto
+            regine = []
 
-            #region segno spazi vietati
-            # Cambio riga a destra
-            while colonna_t < n:
-                if s[riga_t][colonna_t] != "x":
-                    s[riga_t][colonna_t] = "x"
-                    spazi_rimasti -= 1
-                colonna_t +=1
-                    
-            colonna_t = c
-            # Cambio riga a sinistra
-            while colonna_t >= 0:
-                if s[riga_t][colonna_t] != "x":
-                    s[riga_t][colonna_t] = "x"
-                    spazi_rimasti -= 1
-                colonna_t -=1
-            colonna_t = c
-            # Cambio colonna sotto
-            while riga_t < n:
-                if s[riga_t][colonna_t] != "x":
-                    s[riga_t][c] = "x"
-                    spazi_rimasti -= 1
-                riga_t +=1
-            riga_t = r
-            # Cambio colonna sopra
-            while riga_t >= 0:
-                if s[riga_t][colonna_t] != "x":
-                    s[riga_t][c] = "x"
-                    spazi_rimasti -= 1
-                riga_t -=1
-            riga_t = r
-            colonna_t = c
-            # Cambio diagonali sotto
-            while riga_t < n and colonna_t < n:
-                if s[riga_t][colonna_t] != "x":
-                    s[riga_t][colonna_t] = "x"
-                    spazi_rimasti -= 1
-                riga_t +=1
-                colonna_t += 1
-            riga_t = r
-            colonna_t = c
-            while riga_t < n and colonna_t >= 0:
-                if s[riga_t][colonna_t] != "x":
-                    s[riga_t][colonna_t] = "x"
-                    spazi_rimasti -= 1
-                riga_t +=1
-                colonna_t -= 1
-            riga_t = r
-            colonna_t = c
-            # Cambio diagonali sopra
-            while riga_t >= 0 and colonna_t >= 0:
-                if s[riga_t][colonna_t] != "x":
-                    s[riga_t][colonna_t] = "x"
-                    spazi_rimasti -= 1
-                riga_t -=1
-                colonna_t -= 1
-            riga_t = r
-            colonna_t = c
-            while riga_t >= 0 and colonna_t < n:
-                if s[riga_t][colonna_t] != "x":
-                    s[riga_t][colonna_t] = "x"
-                    spazi_rimasti -= 1
-                riga_t -=1
-                colonna_t += 1
-            # Piazzo la regina
-            s[r][c] = "o"
-            piazzate += 1
-            #endregion
+        for j in range(self.regine):                                                     # j scorre da 0 a self.regine (vale sempre n) quindi scorre le colonne in una riga
+            if self.controlloPosizione(regine + [[i,j]], piazzate, scacchiera):          # Ora se il controllo posizione in [i][j] mi da true -> piazzo una regina in [i][j]
+                scacchiera[i][j] = 'o'
+                self.provaPosizione(i + 1, scacchiera, regine + [[i,j]], piazzate + 1)   # Continuo a posizionare quindi dalla riga successiva "(i+1)", sulla stessa scacchiera, ora con
+                scacchiera[i][j] = '.'                                                   # ora con delle regine piazzate e le loro posizioni salvate in "regine"
+    
+    def controlloPosizione(self, regine, piazzate, scacchiera):
+        """
+        Dati in input:
+        regine (una lista delle posizioni delle regine precedentemente piazzate)
+        piazzate (il numero di regine già piazzate)
+        scacchiera (la configurazione corrente della scacchiera)
+        controlla se possono convivere le regine che vorrei piazzare
+        """
+
+        if piazzate < 1:            # Se non ho piazzato manco una regina, ovviamente ritorno true, non ho controlli da fare
+            return True
+        for regina in regine:                               # Se ho invece delle regine piazzate (le loro posizioni sono sempre in "regine")
+            r_x = regina[0]
+            r_y = regina[1]                                 # prendo le coordinate di ognuna nel ciclo e le salvo in "r_x" ed "r_y"
             
-            # Stampo la scacchiera
-            for i in range(n):
-                print(s[i])
-                if i == n-1:
-                    print("\n")
+            for i in range(self.regine):                    # Ciclo da 0 a n
+                if scacchiera[i][r_y] == 'o' and i!=r_x:    # Controllo la riga
+                    return False                            # se trovo un "o", cè una regina, ritorno false
+                if scacchiera[r_x][i] == 'o' and i!=r_y:    # Controllo la colonna
+                    return False                            # se trovo un "o", cè una regina, ritorno false
+                
+            #region Diagonale destra bassa
+            while r_x+1 < self.regine and r_y+1 < self.regine:     # Controllo la diagonale destra bassa
+                r_x += 1                                           # Vado a destra
+                r_y += 1                                           # Vado in basso
+                if scacchiera[r_x][r_y] == 'o':                    # se trovo un "o", cè una regina, ritorno false
+                    return False
+            #endregion
 
-            if piazzate == n:
-                return 1
-        
-        if spazi_rimasti < n-piazzate:
-            # Backtrack all'ultimo stato sicuro
-            return 0
-        elif piazzate == n:
-            # Non ho bisogno di continuare
-            return 1
-        else:
-            # Salvo questo come stato sicuro e re itero
-            return piazzaRegine(r,c+1,n,s,spazi_rimasti)
+            #region Diagonale sinistra bassa
+            r_x = regina[0]                             # Reset, dato che l'ultimo controllo mi ha sfasato
+            r_y = regina[1]
+            while r_x-1 >= 0 and r_y+1 < self.regine:   # Controllo la diagonale sinistra bassa
+                r_x -= 1                                # Vado a sinistra
+                r_y += 1                                # Vado in basso
+                if scacchiera[r_x][r_y] == 'o':         # se trovo un "o", cè una regina, ritorno false
+                    return False
+            #endregion
 
+            #region Diagonale destra alta
+            r_x = regina[0]                                 # Reset, dato che l'ultimo controllo mi ha sfasato
+            r_y = regina[1]
+            while r_x+1 < self.regine and r_y-1 >= 0:       # Controllo la diagonale destra alta
+                r_x += 1                                    # Vado a destra
+                r_y -= 1                                    # Vado in alto
+                if scacchiera[r_x][r_y] == 'o':             # se trovo un "o", cè una regina, ritorno false
+                    return False
+            #endregion  
 
-    combinazioni = provaCombinazione(combinazioni,scacchiera)
+            #region Diagonale sinistra alta
+            r_x = regina[0]                                 # Reset, dato che l'ultimo controllo mi ha sfasato
+            r_y = regina[1]
+            while r_x-1>=0 and r_y-1>=0:                    # Controllo la diagonale sinistra alta
+                r_x -= 1                                    # Vado a sinistra
+                r_y -= 1                                    # Vado in alto
+                if scacchiera[r_x][r_y] == 'o':             # se trovo un "o", cè una regina, ritorno false
+                    return False
+            #endregion
 
-    return combinazioni
-
+        return True     # Se non ho trovato nulla, ovviamente ritorno true     
+            
 if __name__ == "__main__":
     numero = int(input())
-    output = trovaPosizioni(numero)
-    print("SOLUZIONI TROVATE:",output)
+    output = Output()
+    print("SOLUZIONI TROVATE:", output.numeroSoluzioni(numero))
