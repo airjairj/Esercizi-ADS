2f4269e703c21c3ff99b4acdb5f0d7f811b5725d
Falino <alessandrofalino@gmail.com>
2023-10-30 10:07:35 +0100

    HW2

--- HW2/2.1/Soluzione_Es1.py
+++ HW2/2.1/Soluzione_Es1.py
@@ -1,16 +1,20 @@
 def trovaSottoArray(lista):
-        temp = 0
-        tot = float('-inf')
+        """
+        Data una lista in ingresso, ritorna la sottolista con la somma maggiore
+        """
+
+        temp = 0                        # Inizializzo il valore temporaneo che uso come buffer a 0
+        tot = float('-inf')             # Inizializzo il valore che uso come totale a -infinito (0 non andrebbe bene per liste negative)
  
-        for val in lista:
-            temp += val
+        for val in lista:               # Scorro i valori
+            temp += val                 # Sommo il valore al temp
             
-            if temp > tot:
+            if temp > tot:              # Se il temp è maggiore del totale, ho un nuovo totale
                 tot = temp
             
-            if temp < 0:
-                temp = 0
-
+            if temp < 0:                # Se il temp è diventato < 0 dopo l'ultima somma, resetto temp
+                temp = 0                # dato che il maggiore è stato gia salvato, e se è diventato negativo non ci guadagno
+                                        # a continuare questa sottolista, conviene ricominciare
         return tot
 
 
